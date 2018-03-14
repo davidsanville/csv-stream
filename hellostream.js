039d97d7004a3f4980d0a60c96180d5187e8b209
aurelius <davids94@comcast.net>
2018-03-14 02:28:56 -0700

    First commit

---
+++ hellostream.js
@@ -0,0 +1,27 @@
+
+//https://www.tutorialspoint.com/nodejs/nodejs_streams.htm
+
+var fs = require("fs");
+var data = '';
+
+// Create a readable stream
+//var readerStream = fs.createReadStream('input.txt');
+var readerStream = fs.createReadStream('fredGDP.csv');
+
+// Set the encoding to be utf8.
+readerStream.setEncoding('UTF8');
+
+// Handle stream events --> data, end, and error
+readerStream.on('data', function(chunk) {
+   data += chunk;
+});
+
+readerStream.on('end',function(){
+   console.log(data);
+});
+
+readerStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("Program Ended");
